45db0e56cdc3ad1ac8226b3fe81b947fad6bcdab
juvekeeper41 <josb6385@740s-pl-114.local>
2017-01-31 09:29:30 -0700

    Started writing the methods for the timer class.

--- SecondProject/Model/Timer.cpp
+++ SecondProject/Model/Timer.cpp
@@ -6,4 +6,38 @@
 //  Copyright © 2017 Osborne, Jacob. All rights reserved.
 //
 
+#include <iostream>
 #include "Timer.hpp"
+
+using namespace std;
+
+Timer :: Timer()
+{
+    executionTime = 0;
+}
+
+void Timer :: resetTimer()
+{
+    executionTime = 0;
+}
+
+void Timer :: startTimer()
+{
+    executionTime = clock();
+}
+
+void Timer :: stopTimer()
+{
+    executionTime = clock() - executionTime;
+}
+
+void Timer :: displayTimingInformation()
+{
+    cout <<  "The execution time is: " << endl;
+    cout << "In human time it is" << executionTime/CLOCKS_PER_SEC << " seconds" << endl;
+}
+
+long Timer :: getExecutionTimeInMicroseconds()
+{
+    return executionTime;
+}
